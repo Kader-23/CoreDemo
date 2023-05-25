c36c23189108fad6f476f908fd3f41d05f0f3534
Kader gürsoy yılmaz <65540087+Kader-23@users.noreply.github.com>
2023-05-25 11:07:04 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+  public class WriterManager:IWriterService
+    {
+        IWriterDal _writerdal;
+
+        public WriterManager(IWriterDal writerdal)
+        {
+            _writerdal = writerdal;
+        }
+
+        public void WriterAdd(Writer writer)
+        {
+            _writerdal.Insert(writer);
+        }
+
+        public void WriterDelete(Writer writer)
+        {
+
+            _writerdal.Delete(writer);
+        }
+
+        public void WriterUpdate(Writer writer)
+        {
+            _writerdal.Update(writer);
+        }
+
+        public Writer GetById(int id)
+        {
+            return _writerdal.GetById(id);
+        }
+
+        public List<Writer> GetList()
+        {
+            return _writerdal.GetListAll();
+        }
+
+        public List<Writer> GetBlogListByWriter(int id)
+        {
+            throw new NotImplementedException();
+        }
+       
+    }
+}
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,36 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    Username="Kader"
+
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username="Fatih"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="Dila"
+                },
+            };
+            return View(commentvalues);
+        }
+    }
+}
